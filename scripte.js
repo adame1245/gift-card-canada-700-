b5d7f587193c7f43c7b0e051f724f10279291645
adame1245 <100003617+adame1245@users.noreply.github.com>
2022-02-25 14:42:42 -0800

    Add files via upload

---
+++ scripte.js
@@ -0,0 +1,9 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      var button1 = document.getElementById("button1");
+      var button1PressMe = pressMe(button1);
+    },
+    false
+  );
+  
